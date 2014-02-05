2b1ce00d1d5f6a1c99857af4169ceed9ba949009
bigassforce <bigassforce@users.noreply.github.com>
2014-02-05 12:23:47 +0000

    closes #127: expose Configuration in a modal dialogue that shows the View so detail records could be surfaced

--- dev/resource/ChainStepSorterZip/overrides/model/ChainStep.js
+++ dev/resource/ChainStepSorterZip/overrides/model/ChainStep.js
@@ -5,6 +5,8 @@ Ext.define('Override.model.ChainStep', {
     fields.Id.mapping = $ObjectType.ChainStep__c.Fields.Id.Name;
     fields.Name.mapping = $ObjectType.ChainStep__c.Fields.Name.Name;
     fields.Sequence.mapping = $ObjectType.ChainStep__c.Fields.Sequence__c.Name;
+    fields.ConfigurationId.mapping = $ObjectType.ChainStep__c.Fields.ConfigurationId__c.Name;
     fields.ProcessName.mapping = $ObjectType.ChainStep__c.Fields.Process__c.RelationshipName + '.' + $ObjectType.Process__c.Fields.Name.Name;
     fields.ProcessDescription.mapping = $ObjectType.ChainStep__c.Fields.Process__c.RelationshipName + '.'+$ObjectType.Process__c.Fields.Description__c.Name;
+    fields.ProcessIcon.mapping = $ObjectType.ChainStep__c.Fields.Process__c.RelationshipName + '.'+$ObjectType.Process__c.Fields.Icon__c.Name;
 });
